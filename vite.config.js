bd1bcb26bf3b1934699b0d9cec8c08e06cb67612
mohan krishna <2300032862@kluniversity.in>
2025-10-18 08:01:01 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ecommerce-frontend/',
+  base: '/gitpage/', // 👈 MUST match your repo name
 })
